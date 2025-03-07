269ff11773ad10a1c43241f1c88db0466113f6f6
michalbukalski <michalbukalski@gmail.com>
2025-03-07 15:43:57 +0100

    resolved

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,34 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        // Tworzymy nowe konto bankowe
+        BankAccount konto = new BankAccount("123456789", 5000, "Jan Kowalski", "jan.kowalski@example.com", "123-456-789");
+
+        BankAccount konto2 = new BankAccount("123456789123456789", 10000, "Marcin Kowalski", "marcin-kowalski@example.com", "123-456-789");
+
+        // Wyświetlamy informacje początkowe
+        System.out.println("Konto utworzone dla: " + konto.getCustomerName());
+        System.out.println("Początkowe saldo: " + konto.getBalance() + " PLN.");
+
+        // Testujemy wpłatę środków
+        konto.deposit(1500);
+
+        // Testujemy wypłatę środków
+        konto.withdraw(3000);
+
+        // Próba wypłaty większej niż saldo
+        konto.withdraw(5000);
+
+        // Próba wypłaty ujemnej kwoty
+        konto.withdraw(-100);
+
+        System.out.println("Konto utworzone dla: " + konto2.getCustomerName());
+        System.out.println("Początkowe saldo: " + konto2.getBalance() + " PLN.");
+        konto2.deposit(5000);
+        konto2.withdraw(9000);
+        konto2.withdraw(200000);
+
+
+    }
 }
